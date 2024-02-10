b934b307aec0fc64447e433b3dcc3482fc4e4876
requiemcreatif <requiemcreatif@gmail.com>
2024-02-10 21:44:31 +0100

    main page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,96 +1,28 @@
 import Image from "next/image";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-10 py-16">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          
+    <div className=" min-h-screen flex flex-col justify-between">
+      <main className="mx-auto mt-20 px-10 py-12">
+        {/* Header */}
+        <div className="flex flex-col justify-center items-center ">
+          <p className="text-4xl uppercase font-black lg:text-5xl pb-20">DM</p>
+          <h1 className=" text-4xl uppercase font-black lg:text-6xl">
+            Danielle Mani
+          </h1>
+          <p className="text-xl font-light p-2">Coach Sportif Et Bien Être</p>
+          <p className="text-md font-thin py-10 text-center">
+            Le Lorem Ipsum est simplement du faux texte employé dans la
+            composition et la mise en page avant impression. <br /> Le Lorem
+            Ipsum est simplement du faux texte employé dans la composition et la
+            mise en page avant impression.
+          </p>
         </div>
+      </main>
+      <div className=" py-10">
+        <Footer />
       </div>
-
-      <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[380px] after:w-full sm:after:w-[340px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1 className=" text-4xl uppercase font-black">
-          Danielle Mani
-        </h1>
-        <p className="text-xl font-thin p-2">
-          Coach Sportif Et Bien Être
-        </p>
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="#"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-l font-semibold`}>
-            Contact
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            info@daniellemani.com
-          </p>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            0642879373
-          </p>
-        </a>
-
-        <a
-          href="#"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-l font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          53 Boulevard de Pesaro
-          92000 Nanterre
-          </p>
-        </a>
-        <a
-          href="#"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-l font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            
-          </p>
-        </a>
-
-        <a
-          href="#"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-l font-semibold`}>
-            Social
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-           
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
---
+++ components/Footer/index.tsx
@@ -0,0 +1,83 @@
+import { FaInstagram } from "react-icons/fa6";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className=" items-stretch">
+      <div className="mx-auto grid justify-center text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="#"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          // target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-md font-semibold`}>
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              Contact
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            info@daniellemani.com
+          </p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>0642879373</p>
+        </a>
+
+        <a
+          href="#"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          // target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-md font-semibold`}>
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              Localisation{" "}
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            53 Boulevard de Pesaro 92000 Nanterre
+          </p>
+        </a>
+        <a
+          href="#"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          // target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-md font-semibold`}>
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              Siret{" "}
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            53168632700028
+          </p>
+        </a>
+
+        <a
+          href="#"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          // target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-md font-semibold`}>
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              Social
+            </span>
+          </h2>
+          <div
+            className={`m-0 max-w-[30ch] flex gap-2 justify-center font-mono lg:justify-start  text-2xl opacity-50 text-balance`}
+          >
+            <FaInstagram />
+            <FaFacebookSquare />
+            <FaSquareXTwitter />
+            <FaLinkedin />
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
