9432473a9e280a86fc69ed79fa65e91ea5b51ba1
requiemcreatif <requiemcreatif@gmail.com>
2024-02-10 22:42:56 +0100

    anchor tags removed

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -7,12 +7,7 @@ const Footer = () => {
   return (
     <div className=" items-stretch">
       <div className="mx-auto grid justify-center text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href=""
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          // target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
           <h2 className={`mb-3 text-md font-semibold`}>
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               Contact
@@ -22,14 +17,8 @@ const Footer = () => {
             info@daniellemani.com
           </p>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>0642879373</p>
-        </a>
-
-        <a
-          href=""
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          // target="_blank"
-          rel="noopener noreferrer"
-        >
+        </div>
+        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
           <h2 className={`mb-3 text-md font-semibold`}>
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               Localisation{" "}
@@ -38,13 +27,8 @@ const Footer = () => {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             53 Boulevard de Pesaro 92000 Nanterre
           </p>
-        </a>
-        <a
-          href=""
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          // target="_blank"
-          rel="noopener noreferrer"
-        >
+        </div>
+        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
           <h2 className={`mb-3 text-md font-semibold`}>
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               Siret{" "}
@@ -53,28 +37,20 @@ const Footer = () => {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             53168632700028
           </p>
-        </a>
-
-        <a
-          href=""
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          // target="_blank"
-          rel="noopener noreferrer"
-        >
+        </div>
+        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
           <h2 className={`mb-3 text-md font-semibold`}>
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               Social
             </span>
           </h2>
-          <div
-            className={`m-0 max-w-[30ch] flex gap-2 justify-center font-mono lg:justify-start  text-2xl opacity-50 text-balance`}
-          >
+          <div className="m-0 max-w-[30ch] flex gap-2 justify-center font-mono lg:justify-start  text-2xl opacity-50 text-balance">
             <FaInstagram />
             <FaFacebookSquare />
             <FaSquareXTwitter />
             <FaLinkedin />
           </div>
-        </a>
+        </div>
       </div>
     </div>
   );
