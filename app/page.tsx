bf5a531b5fbb2a7340d1489af8af72ce1baf1282
requiemcreatif <requiemcreatif@gmail.com>
2024-02-10 22:34:11 +0100

    text animation added

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,26 @@
-import Image from "next/image";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <div className=" min-h-screen flex flex-col justify-between">
-      <main className="mx-auto mt-20 px-10 py-12">
+      <main className="mx-auto mt-10 px-10 py-12">
         {/* Header */}
         <div className="flex flex-col justify-center items-center ">
-          <p className="text-4xl uppercase font-black lg:text-5xl pb-20">DM</p>
-          <h1 className=" text-4xl uppercase font-black lg:text-6xl">
+          <p className="text-4xl uppercase font-black lg:text-5xl pb-20 slide-down">
+            DM
+          </p>
+          <h1 className=" text-4xl uppercase font-black lg:text-6xl slide-down">
             Danielle Mani
           </h1>
-          <p className="text-xl font-light p-2">Coach Sportif Et Bien Être</p>
-          <p className="text-md font-thin py-10 text-center">
-            Le Lorem Ipsum est simplement du faux texte employé dans la
-            composition et la mise en page avant impression. <br /> Le Lorem
-            Ipsum est simplement du faux texte employé dans la composition et la
-            mise en page avant impression.
+          <p className="text-xl font-light p-2 fade-in">
+            Coach Sportif Et Bien Être
+          </p>
+          <p className="text-lg font-medium pt-8 text-center text-yellow-500 animate-pulse">
+            Site officiel en construction!
           </p>
         </div>
       </main>
-      <div className=" py-10">
+      <div className=" pt-5">
         <Footer />
       </div>
     </div>
